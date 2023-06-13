c315167f8ef5de0b1ebfd00e21073a076abc6016
DouglasSochC <300847474750101@ingenieria.usac.edu.gt>
2023-06-13 14:49:22 -0600

    [ADD] Se ha agregado la conexion bluetooth y ademas el funcionamiento de los botones aceptar y cancelar

--- sketch_principal/Estructuras_Auxiliares.h
+++ sketch_principal/Estructuras_Auxiliares.h
@@ -6,22 +6,23 @@ struct S_Log {
   int siguiente = -1;                                                                                                               // 2 bytes
 };
 
-// Estructura que almacena la informacion de un usuario. 
-// Tamanio de la estructura = 37 bytes
+// Estructura que almacena la informacion de un usuario.
+// Tamanio de la estructura = 39 bytes
 struct S_Usuario {
-  char nombre[13] = { '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };         // 13 bytes
-  char contrasenia[13] = { '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };    // 13 bytes
-  char num_celular[9] = { '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };                             // 9 bytes
-  int siguiente = -1;                                                                                         // 2 bytes
+  char nombre[13] = { '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };       // 13 bytes
+  char contrasenia[13] = { '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };  // 13 bytes
+  char num_celular[9] = { '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };                           // 9 bytes
+  char admin[2] = { '\0', '\0' };                                                                           // 2 bytes
+  int siguiente = -1;                                                                                       // 2 bytes
 };
 
 // Es una estructura auxiliar que ayuda a determinar en que posicion de la memoria EEPROM inicia la lectura de los datos del usuario y log.
 // Ademas, ayuda a determinar en que posicion se puede almacenar un nuevo registro (ini_libre).
 // Tamanio de la estructura = 6 bytes
 struct S_Inicial {
-  int ini_usuario = -1;   // Representa la posicion inicial de lectura de una estructura de tipo usuario; 2 bytes
-  int ini_log = -1;  // Representa la posicion inicial de lectura de una estructura de tipo log; 2 bytes
-  int ini_libre = -1;      // Representa la posicion inicial de guardado de un nuevo registro sin importar su tipo de estructura; 2 bytes
+  int ini_usuario = -1;  // Representa la posicion inicial de lectura de una estructura de tipo usuario; 2 bytes
+  int ini_log = -1;      // Representa la posicion inicial de lectura de una estructura de tipo log; 2 bytes
+  int ini_libre = -1;    // Representa la posicion inicial de guardado de un nuevo registro sin importar su tipo de estructura; 2 bytes
 };
 
 // Valor de los caracteres que posee el teclado
